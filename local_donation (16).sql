ff6fec456b6c3b28bd65709a09b06187a5124b49
James <james.fernandez1239@gmail.com>
2025-12-13 09:09:46 +0800

    s

---
+++ local_donation (16).sql
@@ -0,0 +1,490 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2025 at 02:09 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `local_donation`
+--
+CREATE DATABASE IF NOT EXISTS `local_donation` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `local_donation`;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`activity_id`, `user_id`, `profile_id`, `description`, `display_text`, `created_at`) VALUES
+(1, 1, NULL, 'New user created: James Emmanuel wewew Fernandez (ID: 1)', 'You created a new account: James Emmanuel wewew Fernandez', '2025-12-12 20:51:27'),
+(2, 2, NULL, 'New user created: James Emmanuel Fernandez (ID: 2)', 'You created a new account: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(3, 3, NULL, 'New user created: James Emmanuel Fernandez (ID: 3)', 'You created a new account: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(4, 4, NULL, 'New user created: James Emmanuel Fernandez (ID: 4)', 'You created a new account: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(5, 5, NULL, 'New user created: James Emmanuel Fernandez (ID: 5)', 'You created a new account: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(6, 1, NULL, 'User logged in (ID: 1)', 'You logged in successfully.', '2025-12-13 04:51:54'),
+(7, 2, NULL, 'Admin logged in (ID: 2, Role: Superuser)', 'You logged in successfully.', '2025-12-13 04:52:10'),
+(8, 1, NULL, 'User logged in (ID: 1)', 'You logged in successfully.', '2025-12-13 05:17:17'),
+(9, 1, 1, 'New individual profile created: James Emmanuel wewew Fernandez (Profile ID: 1)', 'You created a new individual profile: James Emmanuel wewew Fernandez', '2025-12-13 05:17:22'),
+(10, NULL, 1, 'User ID 1 was added to Profile ID 1 as owner', 'A member was added with role: owner', '2025-12-13 05:17:22'),
+(11, NULL, 1, 'Added new donation entry #1: request', 'You added a new donation entry request.', '2025-12-13 05:17:40'),
+(12, NULL, 1, 'Added new donation entry #2: offer', 'You added a new donation entry offer.', '2025-12-13 05:18:02'),
+(13, 2, NULL, 'Admin logged in (ID: 2, Role: Superuser)', 'You logged in successfully.', '2025-12-13 05:18:12'),
+(14, 2, NULL, 'Admin logged in (ID: 2, Role: Superuser)', 'You logged in successfully.', '2025-12-13 08:25:06');
+
+--
+-- Dumping data for table `audit_logs`
+--
+
+INSERT INTO `audit_logs` (`activity_id`, `user_id`, `profile_id`, `description`, `created_at`) VALUES
+(1, 1, NULL, 'New user account created: James Emmanuel wewew Fernandez', '2025-12-12 20:51:27'),
+(2, 2, NULL, 'New user account created: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(3, 3, NULL, 'New user account created: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(4, 4, NULL, 'New user account created: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(5, 5, NULL, 'New user account created: James Emmanuel Fernandez', '2025-12-12 20:51:27'),
+(6, 1, 1, 'New member 1added to profile: Profile ID 1', '2025-12-13 05:17:22'),
+(7, 1, 1, 'New profile created: 1', '2025-12-13 05:17:22'),
+(8, NULL, 1, 'Profile ID: 1 added a new donation entry', '2025-12-13 05:17:40'),
+(9, NULL, 1, 'Profile ID: 1 added a new donation entry', '2025-12-13 05:18:02');
+
+--
+-- Dumping data for table `barangays`
+--
+
+INSERT INTO `barangays` (`id`, `city_id`, `name`) VALUES
+(1, 1, 'Palaypay'),
+(2, 1, 'Can-abay'),
+(3, 1, 'Baloog'),
+(4, 1, 'Inuntan'),
+(5, 1, 'Paghirang'),
+(6, 3, 'Bagacay'),
+(7, 3, 'Mercedes'),
+(8, 3, 'Guindapunan'),
+(9, 3, 'Silanga'),
+(10, 3, 'San Andres'),
+(11, 4, 'Rawis'),
+(12, 4, 'Dagum'),
+(13, 4, 'Hamorawon'),
+(14, 4, 'Matobato'),
+(15, 4, 'Gadgaran'),
+(16, 22, 'Barangay 1'),
+(17, 22, 'Barangay 9'),
+(18, 22, 'Barangay 43-B'),
+(19, 22, 'Barangay 74'),
+(20, 22, 'San Jose'),
+(21, 23, 'Linao'),
+(22, 23, 'Cogon'),
+(23, 23, 'Tambulilid'),
+(24, 23, 'Dahilayan'),
+(25, 23, 'Bagong Buhay'),
+(26, 24, 'Arado'),
+(27, 24, 'San Joaquin'),
+(28, 24, 'San Isidro'),
+(29, 24, 'Guindapunan'),
+(30, 24, 'Salvacion'),
+(31, 32, 'Balud'),
+(32, 32, 'Maypangdan'),
+(33, 32, 'Sabang'),
+(34, 32, 'Siha'),
+(35, 32, 'Locsoon'),
+(36, 38, 'Abiera'),
+(37, 38, 'Banahao'),
+(38, 38, 'Barangay 1 Poblacion'),
+(39, 38, 'Barangay 7 Poblacion'),
+(40, 38, 'Cagusipan'),
+(41, 57, 'Acacia'),
+(42, 57, 'Bangkerohan'),
+(43, 57, 'Dalakit'),
+(44, 57, 'Molave'),
+(45, 57, 'Baybay'),
+(46, 60, 'Rawis'),
+(47, 60, 'Bingco'),
+(48, 60, 'Cabagngan'),
+(49, 60, 'Calomotan'),
+(50, 60, 'San Vicente'),
+(51, 77, 'Abgao'),
+(52, 77, 'Asuncion'),
+(53, 77, 'Libertad'),
+(54, 77, 'Mambajao'),
+(55, 77, 'Tagnipa'),
+(56, 93, 'Daha'),
+(57, 93, 'San Roque'),
+(58, 93, 'Santa Cruz'),
+(59, 93, 'Zone 1'),
+(60, 93, 'Zone 3');
+
+--
+-- Dumping data for table `cities`
+--
+
+INSERT INTO `cities` (`id`, `province_id`, `name`) VALUES
+(1, 53, 'Basey'),
+(2, 53, 'Marabut'),
+(3, 53, 'Catbalogan City'),
+(4, 53, 'Calbayog City'),
+(5, 53, 'Daram'),
+(6, 53, 'Gandara'),
+(7, 53, 'Hinabangan'),
+(8, 53, 'Jiabong'),
+(9, 53, 'Matuguinao'),
+(10, 53, 'Motiong'),
+(11, 53, 'Pinabacdao'),
+(12, 53, 'San Jose de Buan'),
+(13, 53, 'San Sebastian'),
+(14, 53, 'Santa Margarita'),
+(15, 53, 'Santa Rita'),
+(16, 53, 'Santo Ni?o'),
+(17, 53, 'Tagapul-an'),
+(18, 53, 'Talalora'),
+(19, 53, 'Tarangnan'),
+(20, 53, 'Villareal'),
+(21, 53, 'Zumarraga'),
+(22, 51, 'Tacloban City'),
+(23, 51, 'Ormoc City'),
+(24, 51, 'Palo'),
+(25, 51, 'Tanauan'),
+(26, 51, 'Tolosa'),
+(27, 51, 'Dulag'),
+(28, 51, 'Burauen'),
+(29, 51, 'Abuyog'),
+(30, 51, 'Jaro'),
+(31, 51, 'Carigara'),
+(32, 50, 'Borongan City'),
+(33, 50, 'Balangkayan'),
+(34, 50, 'Can-avid'),
+(35, 50, 'Dolores'),
+(36, 50, 'General MacArthur'),
+(37, 50, 'Giporlos'),
+(38, 50, 'Guiuan'),
+(39, 50, 'Hernani'),
+(40, 50, 'Jipapad'),
+(41, 50, 'Lawaan'),
+(42, 50, 'Llorente'),
+(43, 50, 'Maslog'),
+(44, 50, 'Maydolong'),
+(45, 50, 'Mercedes'),
+(46, 50, 'Oras'),
+(47, 50, 'Quinapondan'),
+(48, 50, 'Salcedo'),
+(49, 50, 'San Julian'),
+(50, 50, 'San Policarpo'),
+(51, 50, 'Sulat'),
+(52, 50, 'Taft'),
+(53, 52, 'Allen'),
+(54, 52, 'Biri'),
+(55, 52, 'Bobon'),
+(56, 52, 'Capul'),
+(57, 52, 'Catarman'),
+(58, 52, 'Catubig'),
+(59, 52, 'Gamay'),
+(60, 52, 'Laoang'),
+(61, 52, 'Lapinig'),
+(62, 52, 'Las Navas'),
+(63, 52, 'Lavezares'),
+(64, 52, 'Lope de Vega'),
+(65, 52, 'Mapanas'),
+(66, 52, 'Mondragon'),
+(67, 52, 'Palapag'),
+(68, 52, 'Pambujan'),
+(69, 52, 'Rosario'),
+(70, 52, 'San Antonio'),
+(71, 52, 'San Isidro'),
+(72, 52, 'San Jose'),
+(73, 52, 'San Roque'),
+(74, 52, 'San Vicente'),
+(75, 52, 'Silvino Lobos'),
+(76, 52, 'Victoria'),
+(77, 54, 'Maasin City'),
+(78, 54, 'Anahawan'),
+(79, 54, 'Bontoc'),
+(80, 54, 'Hinunangan'),
+(81, 54, 'Hinundayan'),
+(82, 54, 'Libagon'),
+(83, 54, 'Liloan'),
+(84, 54, 'Macrohon'),
+(85, 54, 'Malitbog'),
+(86, 54, 'Padre Burgos'),
+(87, 54, 'Pintuyan'),
+(88, 54, 'Saint Bernard'),
+(89, 54, 'San Francisco'),
+(90, 54, 'San Juan'),
+(91, 54, 'San Ricardo'),
+(92, 54, 'Silago'),
+(93, 54, 'Sogod'),
+(94, 54, 'Tomas Oppus'),
+(95, 49, 'Naval'),
+(96, 49, 'Almeria'),
+(97, 49, 'Biliran'),
+(98, 49, 'Cabucgayan'),
+(99, 49, 'Caibiran'),
+(100, 49, 'Culaba'),
+(101, 49, 'Kawayan'),
+(102, 49, 'Maripipi');
+
+--
+-- Dumping data for table `donation_entries`
+--
+
+INSERT INTO `donation_entries` (`entry_id`, `profile_id`, `entry_type`, `details`, `created_at`, `updated_at`, `target_area`) VALUES
+(1, 1, 'request', 'ddddasd', '2025-12-12 21:17:40', '2025-12-12 21:17:40', 'philippines'),
+(2, 1, 'offer', 'qqqq', '2025-12-12 21:18:02', '2025-12-12 21:18:02', 'region');
+
+--
+-- Dumping data for table `donation_entry_items`
+--
+
+INSERT INTO `donation_entry_items` (`item_entry_id`, `entry_id`, `item_id`, `quantity`, `unit_name`) VALUES
+(1, 1, 17, 3, 'pcs'),
+(2, 1, 6, 1, 'pcs'),
+(3, 2, 7, 5, 'pcs'),
+(4, 2, 19, 3, 'pcs'),
+(5, 2, 14, 6, 'bottles');
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_id`, `item_name`) VALUES
+(21, 'aa'),
+(22, 'bae'),
+(17, 'Batteries'),
+(6, 'Blankets'),
+(9, 'Bottled Water'),
+(10, 'Canned Food'),
+(7, 'Clothes'),
+(13, 'Face Masks'),
+(12, 'First Aid Kits'),
+(16, 'Flashlights'),
+(20, 'Fuel'),
+(18, 'Generators'),
+(11, 'Medical Supplies'),
+(19, 'Radio'),
+(14, 'Sanitizers'),
+(8, 'Shoes'),
+(15, 'Tents');
+
+--
+-- Dumping data for table `item_units`
+--
+
+INSERT INTO `item_units` (`item_unit_id`, `item_id`, `unit_name`) VALUES
+(7, 6, 'pcs'),
+(8, 6, 'pack'),
+(9, 7, 'pcs'),
+(10, 7, 'bundle'),
+(11, 8, 'pairs'),
+(12, 8, 'box'),
+(13, 9, 'bottles'),
+(14, 9, 'case'),
+(15, 10, 'cans'),
+(16, 10, 'box'),
+(17, 11, 'boxes'),
+(18, 11, 'pcs'),
+(19, 12, 'kits'),
+(20, 13, 'pcs'),
+(21, 13, 'box'),
+(22, 14, 'bottles'),
+(23, 14, 'pack'),
+(24, 15, 'pcs'),
+(25, 16, 'pcs'),
+(26, 17, 'pcs'),
+(27, 17, 'pack'),
+(28, 18, 'pcs'),
+(29, 19, 'pcs'),
+(30, 20, 'liters'),
+(31, 20, 'gallons');
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`profile_id`, `user_id`, `profile_type`, `profile_name`, `profile_pic`, `created_at`, `updated_at`) VALUES
+(1, 1, 'individual', 'James Emmanuel wewew Fernandez', 'uploads/1765486686_IMG_20231007_180326.JPG', '2025-12-12 21:17:22', '2025-12-12 21:17:22');
+
+--
+-- Dumping data for table `profiles_individual`
+--
+
+INSERT INTO `profiles_individual` (`profile_id`, `first_name`, `middle_name`, `last_name`, `date_of_birth`, `gender`, `phone_number`, `email`, `region_id`, `province_id`, `city_id`, `barangay_id`, `zip_code`) VALUES
+(1, 'James Emmanuel wewew', 'Palongpong', 'Fernandez', '2001-08-23', 'Male', '09310241773', 'james.fernandez1230@gmail.com', 10, 51, 24, 26, '6720');
+
+--
+-- Dumping data for table `profile_members`
+--
+
+INSERT INTO `profile_members` (`id`, `user_id`, `profile_id`, `role`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'owner', '2025-12-12 21:17:22', '2025-12-12 21:17:22');
+
+--
+-- Dumping data for table `provinces`
+--
+
+INSERT INTO `provinces` (`id`, `region_id`, `name`) VALUES
+(1, 1, 'Abra'),
+(2, 1, 'Apayao'),
+(3, 1, 'Benguet'),
+(4, 1, 'Ifugao'),
+(5, 1, 'Kalinga'),
+(6, 1, 'Mountain Province'),
+(7, 2, 'Ilocos Norte'),
+(8, 2, 'Ilocos Sur'),
+(9, 2, 'La Union'),
+(10, 2, 'Pangasinan'),
+(11, 3, 'Batanes'),
+(12, 3, 'Cagayan'),
+(13, 3, 'Isabela'),
+(14, 3, 'Nueva Vizcaya'),
+(15, 3, 'Quirino'),
+(16, 4, 'Aurora'),
+(17, 4, 'Bataan'),
+(18, 4, 'Bulacan'),
+(19, 4, 'Nueva Ecija'),
+(20, 4, 'Pampanga'),
+(21, 4, 'Tarlac'),
+(22, 4, 'Zambales'),
+(23, 5, 'Batangas'),
+(24, 5, 'Cavite'),
+(25, 5, 'Laguna'),
+(26, 5, 'Quezon'),
+(27, 5, 'Rizal'),
+(28, 6, 'Marinduque'),
+(29, 6, 'Occidental Mindoro'),
+(30, 6, 'Oriental Mindoro'),
+(31, 6, 'Palawan'),
+(32, 6, 'Romblon'),
+(33, 7, 'Albay'),
+(34, 7, 'Camarines Norte'),
+(35, 7, 'Camarines Sur'),
+(36, 7, 'Catanduanes'),
+(37, 7, 'Masbate'),
+(38, 7, 'Sorsogon'),
+(39, 8, 'Aklan'),
+(40, 8, 'Antique'),
+(41, 8, 'Capiz'),
+(42, 8, 'Guimaras'),
+(43, 8, 'Iloilo'),
+(44, 8, 'Negros Occidental'),
+(45, 9, 'Bohol'),
+(46, 9, 'Cebu'),
+(47, 9, 'Negros Oriental'),
+(48, 9, 'Siquijor'),
+(49, 10, 'Biliran'),
+(50, 10, 'Eastern Samar'),
+(51, 10, 'Leyte'),
+(52, 10, 'Northern Samar'),
+(53, 10, 'Samar'),
+(54, 10, 'Southern Leyte'),
+(55, 11, 'Manila'),
+(56, 11, 'Quezon City'),
+(57, 11, 'Makati'),
+(58, 11, 'Pasay'),
+(59, 11, 'Pasig'),
+(60, 11, 'Taguig'),
+(61, 11, 'Caloocan'),
+(62, 11, 'Las Pi?as'),
+(63, 11, 'Muntinlupa'),
+(64, 11, 'Navotas'),
+(65, 11, 'Malabon'),
+(66, 11, 'Mandaluyong'),
+(67, 11, 'Marikina'),
+(68, 11, 'Paranaque'),
+(69, 11, 'San Juan'),
+(70, 11, 'Valenzuela'),
+(71, 11, 'Pateros'),
+(72, 12, 'Zamboanga del Norte'),
+(73, 12, 'Zamboanga del Sur'),
+(74, 12, 'Zamboanga Sibugay'),
+(75, 13, 'Bukidnon'),
+(76, 13, 'Camiguin'),
+(77, 13, 'Lanao del Norte'),
+(78, 13, 'Misamis Occidental'),
+(79, 13, 'Misamis Oriental'),
+(80, 14, 'Davao de Oro'),
+(81, 14, 'Davao del Norte'),
+(82, 14, 'Davao del Sur'),
+(83, 14, 'Davao Occidental'),
+(84, 14, 'Davao Oriental'),
+(85, 15, 'Cotabato'),
+(86, 15, 'Sarangani'),
+(87, 15, 'South Cotabato'),
+(88, 15, 'Sultan Kudarat'),
+(89, 16, 'Agusan del Norte'),
+(90, 16, 'Agusan del Sur'),
+(91, 16, 'Dinagat Islands'),
+(92, 16, 'Surigao del Norte'),
+(93, 16, 'Surigao del Sur'),
+(94, 17, 'Basilan'),
+(95, 17, 'Lanao del Sur'),
+(96, 17, 'Maguindanao'),
+(97, 17, 'Sulu'),
+(98, 17, 'Tawi-Tawi');
+
+--
+-- Dumping data for table `regions`
+--
+
+INSERT INTO `regions` (`id`, `name`) VALUES
+(1, 'CAR - Cordillera Administrative Region'),
+(2, 'Region I - Ilocos Region'),
+(3, 'Region II - Cagayan Valley'),
+(4, 'Region III - Central Luzon'),
+(5, 'Region IV-A - CALABARZON'),
+(6, 'Region IV-B - MIMAROPA'),
+(7, 'Region V - Bicol Region'),
+(8, 'Region VI - Western Visayas'),
+(9, 'Region VII - Central Visayas'),
+(10, 'Region VIII - Eastern Visayas'),
+(11, 'NCR - National Capital Region'),
+(12, 'Region IX - Zamboanga Peninsula'),
+(13, 'Region X - Northern Mindanao'),
+(14, 'Region XI - Davao Region'),
+(15, 'Region XII - SOCCSKSARGEN'),
+(16, 'Region XIII - Caraga'),
+(17, 'BARMM - Bangsamoro Autonomous Region in Muslim Min');
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `profile_pic`, `first_name`, `middle_name`, `last_name`, `date_of_birth`, `gender`, `zip_code`, `phone_number`, `email`, `password`, `role`, `created_at`, `region_id`, `province_id`, `city_id`, `barangay_id`) VALUES
+(1, 'uploads/1765486686_IMG_20231007_180326.JPG', 'James Emmanuel wewew', 'Palongpong', 'Fernandez', '2001-08-23', 'Male', '6720', '09310241773', 'james.fernandez1230@gmail.com', '$2y$10$360P2lsH9hZxXTmyuXpvTOcuCso/Sm9W.fRRj.ooDa7ULhRxKxbAe', 'User', '2025-12-11 12:06:17', 10, 51, 24, 26),
+(2, 'uploads/1765497537_FB_IMG_1698376370669.jpg', 'James Emmanuel', 'Palongpong', 'Fernandez', '2001-08-23', 'Male', '6720', '09310241773', 'admin@gmail.com', '$2y$10$GsorrmO4rp3rhgRQMvH68OGZKLs4mKfxxdbJAHrDBDJbX8/K1MZDm', 'Superuser', '2025-12-11 12:06:36', NULL, NULL, NULL, NULL),
+(3, NULL, 'James Emmanuel', 'Palongpong', 'Fernandez', '2001-08-23', 'Male', NULL, NULL, 'admin3@gmail.com', '$2y$10$/nPU6amabRJRc0/ep/rCXu.nVW9nCCdXTy9ukjVGSvy5gnnmWUpbe', 'User', '2025-12-11 12:07:57', NULL, NULL, NULL, NULL),
+(4, NULL, 'James Emmanuel', 'Palongpong', 'Fernandez', '2003-01-02', 'Male', NULL, NULL, 'james.fernandez1231@gmail.com', '$2y$10$jZ6/I3SNgHP8Iup04orqgO/osAx3AakogieFxIFPtKfGLD5QOEfpi', 'User', '2025-12-11 23:05:43', NULL, NULL, NULL, NULL),
+(5, NULL, 'James Emmanuel', 'Palongpong', 'Fernandez', '2001-08-23', 'Male', NULL, NULL, 'admin2@gmail.com', '$2y$10$ZM4uC30Djbz09gINdCBWd.f8vtttgETyjNv8Q0s7V1WJuSFttAIYe', 'Staff', '2025-12-12 08:41:52', NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_donation_entries`
+--
+DROP TABLE IF EXISTS `view_donation_entries`;
+
+DROP VIEW IF EXISTS `view_donation_entries`;
+CREATE OR REPLACE VIEW `view_donation_entries`  AS SELECT `de`.`entry_id` AS `entry_id`, `de`.`entry_type` AS `entry_type`, `de`.`details` AS `details`, `de`.`created_at` AS `created_at`, `de`.`target_area` AS `target_area`, `p`.`profile_id` AS `profile_id`, `p`.`profile_name` AS `profile_name`, `p`.`profile_type` AS `profile_type`, coalesce(`pi`.`region_id`,`pf`.`region_id`,`pin`.`region_id`,`po`.`region_id`) AS `region_id`, coalesce(`pi`.`province_id`,`pf`.`province_id`,`pin`.`province_id`,`po`.`province_id`) AS `province_id`, coalesce(`pi`.`city_id`,`pf`.`city_id`,`pin`.`city_id`,`po`.`city_id`) AS `city_id`, coalesce(`pi`.`barangay_id`,`pf`.`barangay_id`,`pin`.`barangay_id`,`po`.`barangay_id`) AS `barangay_id`, `r`.`name` AS `region_name`, `pr`.`name` AS `province_name`, `c`.`name` AS `city_name`, `b`.`name` AS `barangay_name` FROM (((((((((`donation_entries` `de` join `profiles` `p` on(`de`.`profile_id` = `p`.`profile_id`)) left join `profiles_individual` `pi` on(`p`.`profile_id` = `pi`.`profile_id` and `p`.`profile_type` = 'individual')) left join `profiles_family` `pf` on(`p`.`profile_id` = `pf`.`profile_id` and `p`.`profile_type` = 'family')) left join `profiles_institution` `pin` on(`p`.`profile_id` = `pin`.`profile_id` and `p`.`profile_type` = 'institution')) left join `profiles_organization` `po` on(`p`.`profile_id` = `po`.`profile_id` and `p`.`profile_type` = 'organization')) left join `regions` `r` on(coalesce(`pi`.`region_id`,`pf`.`region_id`,`pin`.`region_id`,`po`.`region_id`) = `r`.`id`)) left join `provinces` `pr` on(coalesce(`pi`.`province_id`,`pf`.`province_id`,`pin`.`province_id`,`po`.`province_id`) = `pr`.`id`)) left join `cities` `c` on(coalesce(`pi`.`city_id`,`pf`.`city_id`,`pin`.`city_id`,`po`.`city_id`) = `c`.`id`)) left join `barangays` `b` on(coalesce(`pi`.`barangay_id`,`pf`.`barangay_id`,`pin`.`barangay_id`,`po`.`barangay_id`) = `b`.`id`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_recent_feedback`
+--
+DROP TABLE IF EXISTS `view_recent_feedback`;
+
+DROP VIEW IF EXISTS `view_recent_feedback`;
+CREATE OR REPLACE VIEW `view_recent_feedback`  AS SELECT `f`.`feedback_id` AS `feedback_id`, `f`.`feedback` AS `feedback`, `f`.`created_at` AS `created_at`, `f`.`user_id` AS `user_id`, `f`.`profile_id` AS `profile_id`, `u`.`first_name` AS `first_name`, `p`.`profile_name` AS `profile_name`, `p`.`profile_type` AS `profile_type` FROM ((`feedback` `f` left join `users` `u` on(`f`.`user_id` = `u`.`user_id`)) left join `profiles` `p` on(`f`.`profile_id` = `p`.`profile_id`)) ORDER BY `f`.`created_at` DESC ;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
